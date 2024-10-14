d69e1e7b7f52be9a86b2f87761e429044a17c2d5
nikhil <001nikhilshirsath001@gmail.com>
2024-10-14 23:29:47 +0530

    end of crud backend

--- src/main/java/com/nikhilcdac/restapi/controller/StudentController.java
+++ src/main/java/com/nikhilcdac/restapi/controller/StudentController.java
@@ -3,13 +3,19 @@ package com.nikhilcdac.restapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nikhilcdac.restapi.entities.Student;
 import com.nikhilcdac.restapi.service.StudentService;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping("/students")
@@ -25,5 +31,22 @@ public class StudentController {
 	@GetMapping("/student/{id}")
 	public Student getStudent(@PathVariable int id ) {
 		return sservice.getStudent(id);
+	}
+	
+	@PostMapping("/student/add")
+	public Student  addStudent(@RequestBody Student student) {
+		return sservice.addStudent(student);
+	}
+	
+	@PutMapping("student/update/{id}")
+	public Student updateStudent(@PathVariable int id, @RequestBody Student student ) {
+		//TODO: process PUT request
+		return  sservice.updateStudent(id,student);
+		
+	}
+	
+	@DeleteMapping("/student/delete/{id}")
+	public Student deleteStudent(@PathVariable int id) {
+		return sservice.deleteStudent(id);
 	}
 }
--- src/main/java/com/nikhilcdac/restapi/service/StudentService.java
+++ src/main/java/com/nikhilcdac/restapi/service/StudentService.java
@@ -32,4 +32,44 @@ public class StudentService {
 		}
 	}
 
+
+
+	public Student addStudent(Student student) {
+		// TODO Auto-generated method stub
+		Student std  = new Student();
+		System.out.println(student.getStudent_name());
+		std.setStudent_name(student.getStudent_name());
+		std.setStudent_branch(student.getStudent_branch());
+		std.setStudent_percentage(student.getStudent_percentage());
+		return sdao.save(std);
+	}
+
+
+
+	public Student updateStudent(int id, Student st) {
+		// TODO Auto-generated method stub
+		
+		Student student = getStudent(id);
+		if(student!=null) {
+			student.setStudent_name(st.getStudent_name());
+			student.setStudent_branch(st.getStudent_branch());
+			student.setStudent_percentage(st.getStudent_percentage());
+			sdao.save(student);
+			return student;
+		}
+		return null;
+	}
+
+
+
+	public Student deleteStudent(int id) {
+		// TODO Auto-generated method stub
+		Student st = getStudent(id);
+		if(st!=null) {
+			sdao.delete(st);
+			return st;
+		}
+		return null;
+	}
+
 }
